817b9cb3589f7fcc3667b8fcfd0913e5c59fb94d
Simone Sestito <simone@simonesestito.com>
2022-05-05 13:42:07 +0200

    Show response time chart

--- client/chart.c
+++ client/chart.c
@@ -35,7 +35,7 @@ unsigned int min(const unsigned int *data, size_t data_len) {
  * @param data_len Dimensione dell'array
  * @return Il massimo numero trovato
  */
-unsigned int max(const unsigned int *data, size_t data_len)  {
+unsigned int max(const unsigned int *data, size_t data_len) {
     unsigned long max_number = data[0];
     for (int i = 1; i < data_len; i++) {
         if (data[i] > max_number)
@@ -50,52 +50,68 @@ unsigned int max(const unsigned int *data, size_t data_len)  {
  * Utilizza wchar_t, quindi tutte le printf (in tutto il programma)
  * devono essere di tipo WIDE, altrimenti è undefined behaviour.
  *
- * @param data Dati da mostrare
+ * @param original_data Dati da mostrare
  * @param data_len Dimensione dei dati
  */
-void plot_chart(unsigned int *data, size_t data_len) {
+void plot_chart(unsigned int *original_data, size_t data_len) {
     if (data_len == 0)
         return;
+
+    // Pulisci schermo
+    wprintf(L"\e[1;1H\e[2J");
 
     // Ottieni la larghezza del terminale
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    int term_width = w.ws_col;
-    
+    int max_columns = (w.ws_col / 2) - 1;
+
+    // Limita i dati agli ultimi max_columns
+    size_t normalized_data_len = max_columns < data_len ? max_columns : data_len;
+    unsigned int normalized_data[normalized_data_len];
+    size_t data_offset = data_len - normalized_data_len;
+    for (size_t i = 0; i < normalized_data_len; i++) {
+        normalized_data[i] = original_data[data_offset + i];
+    }
+
     // Trova il dato minore e maggiore.
     // Il minimo verrà mostrato ad altezza 1
-    unsigned int min_data = min(data, data_len);
-    unsigned int max_data = max(data, data_len);
-    
+    unsigned int min_data = min(normalized_data, normalized_data_len);
+    unsigned int max_data = max(normalized_data, normalized_data_len);
+
+    // Normalizza i dati in un intervallo [1:10].
+    for (size_t i = 0; i < normalized_data_len; i++) {
+        normalized_data[i] = max_data == min_data
+                             ? 1
+                             : ((normalized_data[i] - min_data) * 9) / (max_data - min_data) + 1;
+    }
+
     wprintf(L"%lc\n", ARROW_UP);
     // Itera sulle righe (altezze) del grafico
-    for (unsigned long curr_height = max_data; curr_height >= min_data; curr_height--) {
+    for (unsigned long curr_height = 10; curr_height >= 1; curr_height--) {
         wprintf(L"%lc", VERTICAL_BAR);
         // Itera sugli elementi dei dati
-        for (int i = 0; i < data_len; i++) {
-            // Mostra il carattere corrispondente alla cella del grafico
-
-            unsigned int previous_element = i == 0 ? 0 : data[i-1];
-            
+        for (size_t i = 0; i < normalized_data_len; i++) {
+            // Mostra il carattere corrispondente alla cella del grafico.
             // Il minimo e massimo tra l'elemento corrente e quello precedente
-            unsigned int min_previous = previous_element < data[i] ? previous_element : data[i];
-            unsigned int max_previous = previous_element > data[i] ? previous_element : data[i];
+            unsigned int previous_element = i == 0 ? 0 : normalized_data[i - 1];
+            unsigned int min_previous = previous_element < normalized_data[i] ? previous_element : normalized_data[i];
+            unsigned int max_previous = previous_element > normalized_data[i] ? previous_element : normalized_data[i];
 
             // Stampa la linea verticale per collegare
             // l'elemento del grafico precedente al corrente
             // e disegnare così una barra verticale
             wchar_t prev_bar_char;
-            if (previous_element == curr_height && curr_height == data[i])
+            if (previous_element == curr_height && curr_height == normalized_data[i])
                 prev_bar_char = HORIZONTAL_BAR; // Sono uguali come altezza
             else if (curr_height < max_previous && curr_height > min_previous)
                 prev_bar_char = VERTICAL_BAR; // Mi trovo tra i due livelli (precedente e corrente)
-            else if (curr_height == max_previous && max_previous == data[i])
+            else if (curr_height == max_previous && max_previous == normalized_data[i])
                 prev_bar_char = CORNER_TOP_LEFT; // Fine della linea ascendente (corrente > precedente)
-            else if (curr_height == max_previous && max_previous == previous_element)
+            else if (curr_height == max_previous)
                 prev_bar_char = CORNER_TOP_RIGHT; // Fine della linea discendente (corrente < precedente)
-            else if (curr_height == min_previous && min_previous == data[i])
+            else if (curr_height == min_previous && min_previous == normalized_data[i])
                 prev_bar_char = CORNER_BOTTOM_LEFT; // Inizio della linea discendente (corrente < precedente)
-            else if (curr_height == min_previous && min_previous == previous_element)
+            else if (curr_height == min_previous)
                 prev_bar_char = CORNER_BOTTOM_RIGHT; // Inizio della linea ascendente (corrente > precedente)
             else
                 prev_bar_char = ' '; // Questa cella deve restare vuota
@@ -103,13 +119,13 @@ void plot_chart(unsigned int *data, size_t data_len) {
             wprintf(L"%lc", prev_bar_char);
 
             // Stampa la linea superiore della barra dell'elemento corrente
-            wprintf(L"%lc", data[i] == curr_height ? HORIZONTAL_BAR : L' ');
+            wprintf(L"%lc", normalized_data[i] == curr_height ? HORIZONTAL_BAR : L' ');
         }
         wprintf(L"\n");
     }
 
     wprintf(L"%lc", CORNER_BOTTOM_LEFT);
-    for (int i = 0; i < term_width - 2; i++)
+    for (int i = 0; i < max_columns*2; i++)
         wprintf(L"%lc", HORIZONTAL_BAR);
     wprintf(L">\n");
 }
--- client/main.c
+++ client/main.c
@@ -1,9 +1,91 @@
 #include <stdlib.h>
+#include <wchar.h>
+#include <unistd.h>
+#include "../common/calc_utils.h"
 #include "../common/socket_utils.h"
 #include "../common/main_init.h"
+#include "chart.h"
+
+/**
+ * Vettore dei tempi delle operazioni, da mostrare nel grafico
+ */
+unsigned int *chart_data = NULL;
+
+/**
+ * Spazio allocato da chart_data
+ */
+size_t chart_data_allocated = 1;
+
+/**
+ * Spazio utilizzato da chart_data (= indice della prossima locazione libera)
+ */
+size_t chart_data_len = 0;
+
+void update_chart(unsigned int new_time);
 
 int main(int argc, const char **argv) {
     // Inizializza
     if (main_init(argc, argv, "client.log", connect_to_server) != 0)
         return EXIT_FAILURE;
+
+    FILE *socket_input = fdopen(socket_fd, "r");
+    FILE *socket_output = fdopen(socket_fd, "w");
+
+    operand_t left_operand, right_operand, result;
+    char operator;
+    unsigned long start_timestamp, end_timestamp;
+
+    while (1 /* FIXME */) {
+        wprintf(L"Prossimo calcolo: ");
+        fflush(stdout);
+
+        if (scanf("%lf %c %lf", &left_operand, &operator, &right_operand) < 3) {
+            // Errore nell'input
+            wprintf(L"Operazione errata\n");
+            // FIXME
+            break;
+        }
+
+
+        fprintf(socket_output, "%c %lf %lf\n", operator, left_operand, right_operand);
+        fflush(socket_output);
+
+        if (fscanf(socket_input, "%lu %lu %lf", &start_timestamp, &end_timestamp, &result) < 3) {
+            perror("fscanf recv operation");
+        } else {
+            // Aggiorna il grafico
+            update_chart(end_timestamp - start_timestamp);
+
+            wprintf(L"[%lu us] %lf %c %lf = %lu\n\n",
+                    end_timestamp - start_timestamp,
+                    left_operand,
+                    operator,
+                    right_operand,
+                    result);
+        }
+    }
+
+    fclose(socket_input);
+    fclose(socket_output);
+    free(chart_data);
+}
+
+/**
+ * Aggiorna il grafico mostrato all'utente sui tempi delle operazioni
+ *
+ * @param new_time Nuovo tempo impiegato
+ */
+void update_chart(unsigned int new_time) {
+    if (chart_data == NULL) {
+        chart_data = calloc(chart_data_allocated, sizeof(unsigned int));
+    }
+
+    if (chart_data_allocated == chart_data_len) {
+        chart_data_allocated *= 2;
+        chart_data = realloc(chart_data, sizeof(unsigned int) * chart_data_allocated);
+    }
+
+    chart_data[chart_data_len++] = new_time;
+
+    plot_chart(chart_data, chart_data_len);
 }
